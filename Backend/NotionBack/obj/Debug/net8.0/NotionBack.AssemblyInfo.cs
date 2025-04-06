2660c22a8ff230aad12aff744752fd3bf59657ee
Anatolii <anatolii.mart6@gmail.com>
2025-04-06 19:30:08 +0300

    >>>

--- Backend/NotionBack/obj/Debug/net8.0/NotionBack.AssemblyInfo.cs
+++ Backend/NotionBack/obj/Debug/net8.0/NotionBack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NotionBack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f4e7cb6bbe5aeeb417966aa9910c0fb560908c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f059f1a50f900c6b2a4e61d40e83fb4961daf91")]
 [assembly: System.Reflection.AssemblyProductAttribute("NotionBack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NotionBack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
