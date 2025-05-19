c756beff19068a0219304a331c396b95ab18eda2
Alesha <121282755+oAleshao@users.noreply.github.com>
2025-05-19 22:22:46 +0300

    Program.cs updated

--- Backend/NotionBack/obj/Debug/net8.0/NotionBack.AssemblyInfo.cs
+++ Backend/NotionBack/obj/Debug/net8.0/NotionBack.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NotionBack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fdd6c8795844686b42567d28817a4ffbe8bf85d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ce4e956033764832048e8513250aeb2566b9f5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("NotionBack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NotionBack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
