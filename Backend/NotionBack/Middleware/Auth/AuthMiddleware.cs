7b1fa1a6f8a7657081d36dc1fdc6c06d47ed36b7
Alesha <121282755+oAleshao@users.noreply.github.com>
2025-05-20 23:15:31 +0300

    updated

--- Backend/NotionBack/Middleware/Auth/AuthMiddleware.cs
+++ Backend/NotionBack/Middleware/Auth/AuthMiddleware.cs
@@ -16,7 +16,7 @@ namespace NotionBack.Middleware.Auth
             var path = httpContext.Request.Path.ToString();
             Console.WriteLine($"\n\n\nAuthMiddleware {DateTime.Now.ToString()} - {path}\n {httpContext.Request.Method} \n\n\n");
 
-            if(path.StartsWith("/imgriff/testing", StringComparison.OrdinalIgnoreCase) || path.StartsWith("/imgriff/auth", StringComparison.OrdinalIgnoreCase))
+            if(path.StartsWith("//imgriff/testing", StringComparison.OrdinalIgnoreCase) || path.StartsWith("//imgriff/auth", StringComparison.OrdinalIgnoreCase))
             {
                 await _next(httpContext);
                 return;
