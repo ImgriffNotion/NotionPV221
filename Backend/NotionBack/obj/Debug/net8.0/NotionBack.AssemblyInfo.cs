a137eee2529dac2e587eaaa95121d66284c7edb9
Alesha <121282755+oAleshao@users.noreply.github.com>
2025-06-04 00:08:02 +0300

    Just pushi=

--- Backend/NotionBack/obj/Debug/net8.0/NotionBack.AssemblyInfo.cs
+++ Backend/NotionBack/obj/Debug/net8.0/NotionBack.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NotionBack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e528bff30d6c1a0470e0a382c4d92a231738938")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+225ed06b3b90c2ae278f277ee939a106a810e716")]
 [assembly: System.Reflection.AssemblyProductAttribute("NotionBack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NotionBack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
